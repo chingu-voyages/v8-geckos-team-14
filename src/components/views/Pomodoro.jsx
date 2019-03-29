f6d307e5acf8dd4c2aceb8e00d560f18502d29c5
Alexever17 <alexever17@gmail.com>
2019-03-29 22:50:54 +0100

    including one input for working type - range

--- src/components/views/Pomodoro.jsx
+++ src/components/views/Pomodoro.jsx
@@ -11,6 +11,39 @@ const Wrapper = styled.div`
   justify-content: center;
 `;
 
+const RangeSlider = styled.input.attrs(({ inputType, min ,max }) => ({
+    type: inputType,
+    min: min,
+    max: max
+}))`
+    -webkit-appearance: none;
+    width: 70%;
+    border-radius: 5px;  
+    margin: 0 0 1em 15%;
+    height: 1em;
+    background: #d3d3d3;
+    outline: none;
+    -webkit-transition: .2s;
+    transition: opacity .2s;
+
+    &::-webkit-slider-thumb {
+        -webkit-appearance: none;
+        appearance: none;
+        border-radius: 50%;  
+        width: 25px;
+        height: 25px;
+        background: #fff;
+        cursor: pointer;
+    }
+
+    &::-moz-range-thumb {
+        width: 25px;
+        height: 25px;
+        background: #fff;
+        cursor: pointer;
+    }
+`
+
 //the number which display the minutes remaining to work
 const Display = styled.h1`
   color: white;
@@ -47,24 +80,12 @@ export default class Pomodoro extends Component {
     constructor() {
         super();
         this.state = {
-            text: "Click the Button",
+            text: "Slide the timer to set your working time",
             button: <TomatoButton onClick={e => this.startTicking(e)}>Start</TomatoButton>,
-            //duration of working in milliseconds
             duration: 1500000,
-            //duration of the seconds in milliseconds
             break: 300000,
-            //will containt the Date object as a reference to precisely calculate the seconds
             start: null,
-            time: {
-                // returns the difference between now and when the pomodoro was started
-                delta: () => { return Math.round((this.state.start - Date.now()) * -1) },
-                // returns the value of the pomodoro working time and time difference between start and now
-                elapsing: () => { return this.state.duration - this.state.time.delta(); },
-                minutes: () => { return Math.floor(this.state.time.elapsing() / 1000 / 60)},
-                seconds: () => { return this.secondsFormatter(Math.floor(this.state.time.elapsing() / 1000 % 60))}
-            },
-            //output to the screen
-            display: () => { return [Math.floor(this.state.duration / 1000 / 60), "00"] }
+            display: () => { return Math.floor(this.state.duration / 1000 / 60).toString() + ":00"}
         };
     }
 
@@ -72,8 +93,14 @@ export default class Pomodoro extends Component {
         return (
             <Wrapper>
                 <Explainer>{this.state.text}</Explainer>
-                <Display>{this.state.display()[0] + ":" + this.state.display()[1]}</Display>
+                <RangeSlider
+                    type="range"
+                    min="1"
+                    max="59"
+                    onChange="" />
+                <Display>{this.state.display()}</Display>
                 <div>{this.state.button}</div>
+                <Reset>Reset</Reset>
             </Wrapper>
         );
     }
@@ -101,9 +128,16 @@ export default class Pomodoro extends Component {
         if (this.state.start === null) {
             this.setState({ start: Date.now() })
         } else {  
-            this.setState({ display: () => { return [this.state.time.minutes(), this.state.time.seconds()]  }})
+            this.setState({ display: () => { return this.minutes().toString() + ":" + this.seconds().toString() }})
         }
     }
+
+
+    // ------------------ HELPING FUNCTIONS --------------------------------------------------------------------------------
+
+
+    //output to the screen
+    startMinutesDisplay = (duration) => { }
 
     //if the seconds which is needed to be display is only in 0 didgets it gets a 0 before it.
     //1 --> 01 ; 59 --> 59
@@ -111,4 +145,16 @@ export default class Pomodoro extends Component {
         let seconds = currentTime.toString().length == 1 ? "0" + currentTime.toString() : currentTime;
         return seconds
     }
+
+    // returns the difference between now and when the pomodoro was started
+    tickingDelta = () => { return Math.round((this.state.start - Date.now()) * -1) }
+    
+    // returns the value of the pomodoro working time and time difference between start and now
+    elapsing = () => { return this.state.duration - this.tickingDelta(); }
+    
+    //gives back the minutes left in teh ticking time
+    minutes = () => { return Math.floor(this.elapsing() / 1000 / 60) }
+
+    //gives back the seconds left in teh ticking time
+    seconds = () => { return this.secondsFormatter(Math.floor(this.elapsing() / 1000 % 60)) }
 }
